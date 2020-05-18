33742bfd242472c6e1213b4f555dce2191a7204c
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-18 10:25:09 -0700

    added function to handle repository addition

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,67 +1,57 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
+import api from '../../services/api';
 import logo from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const regex = /.{3,}\/.{1,}/;
+    const isValid = new RegExp(regex).test(newRepo);
+
+    if (isValid) {
+      const response = await api.get<Repository>(`/repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+    }
+  }
+
   return (
     <>
       <img src={logo} alt="Github Explorer" />
       <Title>Explore Github Repositories</Title>
 
-      <Form>
-        <input type="text" placeholder="Type the repository's name" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          type="text"
+          placeholder="Type the repository's name"
+        />
         <button type="submit">Search</button>
       </Form>
 
       <Repositories>
-        <a href="test">
-          <img
-            src="https://avatars3.githubusercontent.com/u/11969565?s=460&u=87d76ecd6761fd3604e618a9318a107d7957dfc3&v=4"
-            alt="Juliani Schlickmann"
-          />
-          <div>
-            <strong>schlickmann/be-the-hero</strong>
-            <p>
-              Application created to help Non-Governmental Organization's to
-              share their cases
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars3.githubusercontent.com/u/11969565?s=460&u=87d76ecd6761fd3604e618a9318a107d7957dfc3&v=4"
-            alt="Juliani Schlickmann"
-          />
-          <div>
-            <strong>schlickmann/be-the-hero</strong>
-            <p>
-              Application created to help Non-Governmental Organization's to
-              share their cases
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars3.githubusercontent.com/u/11969565?s=460&u=87d76ecd6761fd3604e618a9318a107d7957dfc3&v=4"
-            alt="Juliani Schlickmann"
-          />
-          <div>
-            <strong>schlickmann/be-the-hero</strong>
-            <p>
-              Application created to help Non-Governmental Organization's to
-              share their cases
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
         <a href="test">
           <img
             src="https://avatars3.githubusercontent.com/u/11969565?s=460&u=87d76ecd6761fd3604e618a9318a107d7957dfc3&v=4"
