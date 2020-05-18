c89fcaa91b8e2b2a8989421b9aee7ad67fed1dd1
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-18 14:52:26 -0700

    started stylization of repository route

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,12 +1,64 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
+import logo from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
 }
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Return to Dashboard
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars3.githubusercontent.com/u/11969565?s=460&u=87d76ecd6761fd3604e618a9318a107d7957dfc3&v=4"
+            alt=""
+          />
+          <div>
+            <strong>schlickmann/test</strong>
+            <p>Descri</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1800</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>49</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>30</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="asdad">
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
