ae6801daf4cb825001cc038ced1d45d0726ebade
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-17 17:21:02 -0700

    added styled-components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 // import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return <h1>Explore Github Repositories</h1>;
 };
 
 export default Dashboard;
